aeb687e7a97e5a9efcffaabebfbf79996d097b39
Michael Truog <mjtruog@gmail.com>
2017-06-01 09:55:00 -0700

    Change license from BSD-4-Clause to MIT

--- README.markdown
+++ README.markdown
@@ -16,5 +16,5 @@ Michael Truog (mjtruog [at] gmail (dot) com)
 License
 -------
 
-BSD
+MIT License
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mapsd.Mixfile do
 
   def project do
     [app: :mapsd,
-     version: "0.1.0",
+     version: "0.2.0",
      language: :erlang,
      description: description,
      package: package,
@@ -25,7 +25,7 @@ defmodule Mapsd.Mixfile do
   defp package do
     [files: ~w(src rebar.config README.markdown LICENSE),
      maintainers: ["Michael Truog"],
-     licenses: ["BSD"],
+     licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/okeuday/mapsd"}]
    end
 end
